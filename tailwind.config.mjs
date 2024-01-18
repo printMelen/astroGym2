ecd677bc7fad08c01fd0e479540ff15e30943270
Alvaro Redondo Rodriguez <alvaro.redrod.1@educa.jcyl.es>
2024-01-18 15:56:25 +0100

    Archivos del anterior repo v1

---
+++ tailwind.config.mjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+// const { addDynamicIconSelectors } = require('@iconify/tailwind');
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gym': "url('/gym-back.webp')",
+				'gymCover': "url('public/gym-back.webp')",
+				'silver': "url('/silver.jpg')",
+			}
+		},
+		colors:{
+			'casiNegro': '#10002B',
+			'casiNegrolite': '#240046',
+			'casiNegroMedio': '#3C096C',
+			'moradoOscuro': '#5A189A',
+			'morado': '#7B2CBF',
+			'purpura': '#9D4EDD',
+			'purpuraClaro': '#C77DFF',
+			'claro': '#E0AAFF',
+			'white': '#FFF',
+		}
+	},
+	plugins: [
+		// addDynamicIconSelectors(),
+	],
+};
+
