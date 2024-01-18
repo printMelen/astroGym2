107882f7f08c057af9c768f3019af681d67ceff4
Alvaro Redondo Rodriguez <alvaro.redrod.1@educa.jcyl.es>
2024-01-18 16:08:28 +0100

    Probando el deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://printMelen.github.io',
+  base: '/astroGym2',
   integrations: [tailwind()]
 });
